51eceea8ffd2b5217ea8560240c24107c1fbac32
Camille PECH <camille.pech@epfedu.fr>
2023-10-23 17:15:16 +0200

    TP3 camille pech :maj personnage

--- TP3_Heroic_Fantasy_PECH/src/Personnages/Personnage.java
+++ TP3_Heroic_Fantasy_PECH/src/Personnages/Personnage.java
@@ -4,6 +4,9 @@
  */
 package Personnages;
 
+import Armes.Arme;
+import java.util.ArrayList;
+
 /**
  *
  * @author Camille
@@ -11,15 +14,40 @@ package Personnages;
 public abstract class Personnage {
     String nom;
     int niveauVie;
-
-    public Personnage(String nom, int niveauVie) {
+    ArrayList<Arme> inventaire  = new ArrayList<Arme>();
+    private Arme Arme_en_Main;
+    
+     public void Ajouter_Arme (Arme armedonnée){
+        int taille3=inventaire.size();
+        if (taille3 < 5){
+        inventaire.add(armedonnée);
+        }
+     }    
+    public Personnage(String nom, int niveauVie, Arme Arme_en_Main) {
         this.nom = nom;
         this.niveauVie = niveauVie;
+        this.Arme_en_Main = null;
     }
 
+    public Arme getArme_en_Main() {
+        return Arme_en_Main;
+    }
+    public void équiper_arme (String nv_arme){
+        for (int i=0 ; i<inventaire.size() ; i++){
+            if (inventaire.get(1).equals(nv_arme)){
+                Arme_en_Main=inventaire.get(i);
+                System.out.println("L'arme à été trouvé et équipée");
+            
+        }
+        
+    
+    
+    
+    }
+    }
     @Override
     public String toString() {
-        return "Personnage" + "nom=" + nom + ", niveauVie=" + niveauVie + '}';
+        return "Personnage" + "nom=" + nom + ", niveauVie=" + niveauVie + "arme en main :" +Arme_en_Main;
     }
     
     
