243c1c8f2b68863580a56122167c27181336d6d3
Camille PECH <camille.pech@epfedu.fr>
2023-10-24 17:44:09 +0200

    TP3 camille pech :debut interface

---
+++ TP3_Heroic_Fantasy_PECH/src/tp3_heroic_fantasy_pech/etreVivant.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tp3_heroic_fantasy_pech;
+
+/**
+ *
+ * @author Camille
+ */
+public interface etreVivant {
+    public void seFatiguer();
+    boolean estVivant();
+    public void estAttaqué(int points);
+            
+}
